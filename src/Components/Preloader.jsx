11600eb78623160c05f064531577a18dfa513f2b
Aswin-Biju03 <aswinbiju0303@gmail.com>
2026-04-07 11:31:45 +0530

    Created Header and defined routes for pages

---
+++ src/Components/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Preloader() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <img width={'250px'} src="https://cdn-icons-gif.flaticon.com/12743/12743734.gif" alt="Loading" />
+    </div>
+  );
+}
+
+export default Preloader;
---
+++ src/Pages/Pnf.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Pnf() {
+  return (
+    <div>
+      <img
+        src="https://png.pngtree.com/png-clipart/20220109/original/pngtree-404-error-page-not-found-png-image_7021505.png"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Pnf;
