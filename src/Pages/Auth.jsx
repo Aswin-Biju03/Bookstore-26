8580b83b2b6b6420579ecfa38222006a49b8e329
Aswin-Biju03 <aswinbiju0303@gmail.com>
2026-04-08 11:27:03 +0530

    Designed contact,login,register pages

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-function Auth() {
+function Auth({ insideRegister }) {
+  const [togglePasswordType, setTogglePasswordType] = useState(false);
   return (
-    <div>Auth</div>
-  )
+    <div className="w-full min-h-screen flex justify-center items-center bg-[url(/background.png)] bg-cover bg-center text-white">
+      <div className="p-10">
+        <h1 className="text-center font-bold text-3xl">BOOK STORE</h1>
+        <div
+          style={{ width: "400px" }}
+          className="bg-black text-white p-5 flex justify-center items-center flex-col my-5 rounded-xl"
+        >
+          <div
+            style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+            className="border mb-5 flex justify-center items-center"
+          >
+            <FaUser className="text-3xl" />
+          </div>
+          <h1 className="text-2xl">{insideRegister ? "Register" : "Login"}</h1>
+          <form className="my-5 w-full">
+            {insideRegister && (
+              <input
+                className="bg-white p-2 w-full rounded my-3 text-black"
+                type="text"
+                placeholder="Username"
+              />
+            )}
+            <input
+              className="bg-white p-2 w-full rounded my-3 text-black"
+              type="text"
+              placeholder="Email"
+            />
+            <div className="flex items-center">
+              <input
+                className="bg-white p-2 w-full rounded my-3 text-black"
+                type={togglePasswordType ? "text" : "password"}
+                placeholder="Password"
+              />
+              {togglePasswordType ? (
+                <FaEyeSlash
+                  onClick={() => setTogglePasswordType(!togglePasswordType)}
+                  style={{ marginLeft: "-30px" }}
+                  className="text-gray-500 cursor-pointer"
+                />
+              ) : (
+                <FaEye
+                  onClick={() => setTogglePasswordType(!togglePasswordType)}
+                  style={{ marginLeft: "-30px" }}
+                  className="text-gray-500 cursor-pointer"
+                />
+              )}
+            </div>
+            <div className="flex justify-between mb-5">
+              <p className="text-xs text-orange-300">
+                Never Share Your Password With Others
+              </p>
+              {!insideRegister && (
+                <button className="text-xs underline">Forgot Password</button>
+              )}
+            </div>
+            <div className="text-center">
+              {insideRegister ? (
+                <button className="bg-green-600 p-2 w-full rounded">
+                  Register
+                </button>
+              ) : (
+                <button className="bg-green-600 p-2 w-full rounded">
+                  Login
+                </button>
+              )}
+            </div>
+            {!insideRegister && (
+              <div className="my-5 text-center">
+                <p>--------------------or --------------------</p>
+                <div className="mt-2 flex justify-center items-center w-full">
+                  Google Authentication
+                </div>
+              </div>
+            )}
+            <div className="mt-5 text-center">
+              {insideRegister ? (
+                <p className="text-blue-500"> Existing User ?<Link to={"/login"} className="underline ms-5"> Login</Link>{" "}</p>
+              ) : (
+                <p className="text-blue-500">
+                  New User ?<Link to={"/register"} className="underline ms-5"> Register</Link>{" "}
+                </p>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Auth
+export default Auth;
--- src/user/pages/Home.jsx
+++ src/user/pages/Home.jsx
@@ -1,12 +1,117 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../../Components/Footer";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
       <Header />
-      Home
+      <div
+        style={{ height: "600px" }}
+        className="flex flex-col justify-center items-center bg-[url(/background.png)] bg-cover text-white"
+      >
+        <div
+          style={{ height: "600px", backgroundColor: "rgba(0,0,0,0.3)" }}
+          className="w-full flex flex-col justify-center items-center"
+        >
+          <h1 className="text-6xl font-bold">Wonderful Gifts</h1>
+          <p>Gift your family and friends a book</p>
+          <div className="mt-9 flex items-center">
+            <input
+              type="text"
+              placeholder="Search a book"
+              className="bg-white p-2 rounded-3xl w-100 text-black"
+            />
+            <FaSearch
+              className="text-gray-500 cursor-pointer"
+              style={{ marginLeft: "-40px" }}
+            />
+          </div>
+        </div>
+      </div>
+      <section className="md:px-40 my-5 p-5 flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold my-3">NEW ARRIVALS</h1>
+        <h1 className="text-4xl my-2">Explore Our Latest Collection</h1>
+        <div className="md:grid grid-cols-4 w-full my-10">
+          <div className="shadow rounded p-3 m-4 md:my-0">
+            <img
+              width={"100%"}
+              height={"300px"}
+              src="https://m.media-amazon.com/images/I/81R2N4PRuUL._AC_UF1000,1000_QL80_.jpg"
+              alt="book"
+            />
+            <div className="flex flex-col justify-center items-center mt-4">
+              <h2 className="text-blue-700 font-bold text-xl">Author</h2>
+              <h3 className="text-lg">Title</h3>
+              <p className="font-bold text-red-500">price</p>
+            </div>
+          </div>
+        </div>
+        <div className="text-center my-10">
+          <Link to={"/books"} className="bg-black text-white font-bold p-3">
+            Explore More...
+          </Link>
+        </div>
+      </section>
+      <section className="md:grid grid-cols-2 items gap-10 p-5 md:px-40">
+        <div className="text-center">
+          <h2 className="text-xl font-bold">FEATURED AUTHORS</h2>
+          <h3 className="text-xl">Captivates with every word</h3>
+          <p className="my-5 text-justify">
+            Welcome to the Author Spotlight section of our bookstore website!
+            This feature is designed to celebrate writers, showcase their
+            creative journeys, and help readers discover the minds behind their
+            favorite books.
+          </p>
+          <p className="text-justify">Our Author Features include:</p>
+          <p className="text-justify my-5">
+            <span className="font-bold">✨ Author Profiles :</span> Get to know
+            each author through detailed profiles that highlight their
+            biography, writing style, achievements, and personal inspirations.
+          </p>
+          <p className="text-justify my-5">
+            <span className="font-bold">📖 Published Works :</span> Explore a
+            curated list of books written by the author with quick access to
+            book details, reviews, and purchase options.
+          </p>
+          <p className="text-justify my-5">
+            <span className="font-bold">🎤 Interviews & Insights :</span>{" "}
+            Exclusive interviews, behind-the-scenes stories, and writing tips
+            that offer a deeper look into the author’s creative world.
+          </p>
+        </div>
+        <div className="p-5 flex items-center justify-center">
+          <img
+            style={{ width: "50%" }}
+            src=" https://img.freepik.com/free-photo/portrait-beautiful-woman-enjoying-cup-coffee-while-woking_23-2148937756.jpg?semt=ais_rp_progressive&w=740&q=80"
+            alt="author"
+          />
+        </div>
+      </section>
+      <section className="md:px-40 my-5 p-5 flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold my-3">TESTIMONIAL</h1>
+        <h1 className="text-4xl my-4">See What Others are Saying</h1>
+        <div className="flex flex-col justify-center items-center">
+          <img
+            width={"200px"}
+            height={"200px"}
+            src="https://www.photoforid.com/static/images/thumbnail.jpg"
+            alt=""
+            style={{ borderRadius: "50%" }}
+          />
+          <h3 className="text-center my-4">David Harbour</h3>
+          <p className="text-center">
+            This bookstore has completely changed the way I discover new books.
+            The recommendations are always spot-on, and the delivery is super
+            fast. I love the clean interface and the huge collection! The user
+            experience is amazing! Easy navigation, great deals, and beautifully
+            organized categories. I appreciate how quickly customer support
+            responds too.
+          </p>
+        </div>
+      </section>
       <Footer />
     </>
   );
